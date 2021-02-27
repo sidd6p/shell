aaa60478140f8f903dfc995be5fe12bdf24ebe52
siddhartha <siddpurwar6@gmail.com>
2021-02-27 19:05:52 +0530

    about to start a1

--- cp_check.sh
+++
@@ -1,9 +0,0 @@
-echo "Enter source and destination file"
-read source
-read destination
-if cp $source $destination//doubt
-then
-  echo "File copying successfull"
-else
-  echo "file copying Not successful"
-fi
